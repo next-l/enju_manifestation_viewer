cd8fb2c3ad77b79e3349669f1e3c59e038de5430
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-12 00:58:40 +0900

    added generator

---
+++ lib/generators/enju_manifestation_viewer/setup/setup_generator.rb
@@ -0,0 +1,8 @@
+class EnjuManifestationViewer::SetupGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+
+  def setup
+    rake("enju_purchase_request_engine:install:migrations")
+    inject_into_class 'app/models/manifestation.rb', Manifestation,
+      "  enju_manifestation_viewer\n"
+end
