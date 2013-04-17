35e3c89ca682fabb012769db1e9ffd00ec96513d
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-17 11:05:53 +0900

    removed generator

--- lib/generators/enju_manifestation_viewer/setup/setup_generator.rb
+++
@@ -1,8 +0,0 @@
-class EnjuManifestationViewer::SetupGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
-
-  def setup
-    rake("enju_purchase_request_engine:install:migrations")
-    inject_into_class 'app/models/manifestation.rb', Manifestation,
-      "  enju_manifestation_viewer\n"
-end
