8b5206a09193ab3b9e7d356e38d6ae7453008cb4
Kosuke Tanabe <nabeta@fastmail.fm>
2017-01-10 00:29:43 +0900

    update dummy migration files

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  enju_leaf
+  include EnjuBiblio::Controller
+  include EnjuLibrary::Controller
+  before_action :set_paper_trail_whodunnit
+  after_action :verify_authorized
+
+  include Pundit
 end
