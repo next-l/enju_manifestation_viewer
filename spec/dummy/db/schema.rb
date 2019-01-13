ca7b41c3f9b2042f3d984f8e6851160f7091cb4c
Kosuke Tanabe <nabeta@fastmail.fm>
2019-01-14 03:31:30 +0900

    update version of migration files

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -209,7 +209,7 @@ ActiveRecord::Schema.define(version: 2019_01_02_034126) do
     t.datetime "updated_at"
     t.string "attachment_file_name"
     t.string "attachment_content_type"
-    t.integer "attachment_file_size"
+    t.bigint "attachment_file_size"
     t.datetime "attachment_updated_at"
   end
 
@@ -497,7 +497,7 @@ ActiveRecord::Schema.define(version: 2019_01_02_034126) do
     t.boolean "csv_charset_conversion", default: false, null: false
     t.string "header_logo_file_name"
     t.string "header_logo_content_type"
-    t.integer "header_logo_file_size"
+    t.bigint "header_logo_file_size"
     t.datetime "header_logo_updated_at"
     t.text "header_logo_meta"
     t.index ["short_name"], name: "index_library_groups_on_short_name"
@@ -731,7 +731,7 @@ ActiveRecord::Schema.define(version: 2019_01_02_034126) do
     t.integer "user_id"
     t.string "resource_export_file_name"
     t.string "resource_export_content_type"
-    t.integer "resource_export_file_size"
+    t.bigint "resource_export_file_size"
     t.datetime "resource_export_updated_at"
     t.datetime "executed_at"
     t.datetime "created_at"
@@ -905,7 +905,7 @@ ActiveRecord::Schema.define(version: 2019_01_02_034126) do
     t.integer "user_id"
     t.string "user_export_file_name"
     t.string "user_export_content_type"
-    t.integer "user_export_file_size"
+    t.bigint "user_export_file_size"
     t.datetime "user_export_updated_at"
     t.datetime "executed_at"
     t.datetime "created_at"
