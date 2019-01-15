bd38f83cbcf104b5b0c692dfad8cc8ed9a313fcd
Kosuke Tanabe <nabeta@fastmail.fm>
2019-01-15 12:23:55 +0900

    fix loading module

---
+++ spec/dummy/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  devise :database_authenticatable, #:registerable,
+    :recoverable, :rememberable, :trackable, #, :validatable
+    :lockable, lock_strategy: :none, unlock_strategy: :none
+
+  include EnjuSeed::EnjuUser
+end
+
+Manifestation.include(EnjuManifestationViewer::EnjuManifestation)
